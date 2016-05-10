2e5216b4a5fc2aa51c2b75e516405c7f53e79063
Fu_sionWorkMac <Fu_sion@live.com>
2016-05-10 09:13:06 +0800

    新建Run项目

---
+++ Runer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Runer
+//
+//  Created by tarena on 16/5/10.
+//  Copyright © 2016年 Fu_sion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Runer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Runer
+//
+//  Created by tarena on 16/5/10.
+//  Copyright © 2016年 Fu_sion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
